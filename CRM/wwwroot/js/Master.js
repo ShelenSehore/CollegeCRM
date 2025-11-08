996e3e5278a644b7e63456b71bac04e117c80d94
ShelenSehore <57551189+ShelenSehore@users.noreply.github.com>
2025-11-08 12:45:19 -0800

    worked on start project create html template

---
+++ CRM/wwwroot/js/Master.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    let menuicn = document.querySelector(".menuicn");
+    let nav = document.querySelector(".navcontainer");
+
+    menuicn.addEventListener("click", () => {
+
+        nav.classList.toggle("navclose");
+    })
+
+
+
+
+});
